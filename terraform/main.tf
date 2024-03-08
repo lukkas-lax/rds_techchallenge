1260c93f8d6fbc031143a5dec968f6c529420c58
lukit <lukkitaalves6@gmail.com>
2024-03-08 20:38:33 -0300

    feat: Adicioan github actions

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,9 +1,7 @@
 #main.tf
 #defining the provider as aws
 provider "aws" {
-    region     = "${var.region}"
-    access_key = "${var.access_key}"
-    secret_key = "${var.secret_key}"
+    region     = "sa-east-1"
 }
 
 #create a security group for RDS Database Instance
@@ -30,8 +28,8 @@ resource "aws_db_instance" "mysqltechchallenge" {
   allocated_storage    =  20
   engine_version       = "5.7"
   instance_class       = "db.t2.micro"
-  username             = "root"
-  password             = "170593"
+  username             = "${ env.USER_MYSQL }"
+  password             = "${ secrets.PASSWORD_MYSQL }"
   parameter_group_name = "default.mysql5.7"
   vpc_security_group_ids = ["${aws_security_group.rds_sg.id}"]
   skip_final_snapshot  = true
